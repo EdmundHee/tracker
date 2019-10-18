5d06ebc8af76d70cfcc1559d551783ee89b1963e
Edmund Hee <edmund.hee05@gmail.com>
2019-10-18 15:59:34 +0800

    updated snail.js

--- snail.js
+++ snail.js
@@ -4,12 +4,18 @@ const generateUserId = () => generateS4() + generateS4() + generateS4() + genera
 const generateSessionId = () => new Date().getTime();
 
 var Snail = (configurations) => {
+    // **************************************************************************************************     
+    //  Variable to Snail
+    // **************************************************************************************************
     let windowAlias = window;
     let hostname = windowAlias.location.hostname;
     let Protocol = windowAlias.location.protocol;
     let pttOpen = windowAlias.XMLHttpRequest.prototype.open;
     let pttSend = windowAlias.XMLHttpRequest.prototype.send;
 
+    // **************************************************************************************************     
+    //  Check for id
+    // **************************************************************************************************
     let checkEgg = hostname => {return localStorage.getItem(hostname);};
     
     let layEgg = (hostname, content=(generateUserId()+":"+generateSessionId())) => {
@@ -30,56 +36,92 @@ var Snail = (configurations) => {
     let egg = updateEgg(hostname, grabEgg(hostname));
 
     console.log(egg);
+    // **************************************************************************************************     
+    //  Click Event Listener
+    // **************************************************************************************************
+
+    document.onclick = (e) =>{
+        // console.log(e);
+        // console.log(window.event.srcElement);
+        // console.log(e.target);
+        let click = (window.event) ? window.event.srcElement : e.target;
+        let tags = document.getElementsByTagName(click.tagName);
+
+        for(var i=0;i<tags.length;++i){
+            if(tags[i]==click)
+            {
+                console.log(tags[i]);
+                console.log(click.tagName);
+            }    
+        }
+    };
+    // **************************************************************************************************     
+    //  Cursor Movement Tracker Listener
+    // **************************************************************************************************
+
+    (() => {
+        window.onmousemove = function(e){
+            // console.log(`Movement ${e.screenX},${e.screenY} - ${new Date().getTime()}`);
+        }
+     })();
+
+
+    // **************************************************************************************************     
+    //  API Event Listener
+    // **************************************************************************************************
+    // let windowAlias = window;
+    // let pttOpen = windowAlias.XMLHttpRequest.prototype.open;
+    // let pttSend = windowAlias.XMLHttpRequest.prototype.send;
 
     let startMunch = (() => {
-        window.XMLHttpRequest.prototype.open = function() {
-            console.log( arguments );
-            return pttOpen.apply(this, [].slice.call(arguments));
-        };
-    });
-
-    let startDigest = (() => {
-        window.XMLHttpRequest.prototype.send = function() {
-            console.log( arguments );
+        window.XMLHttpRequest.prototype.send = function(){
+            console.log(arguments)
             return pttSend.apply(this, [].slice.call(arguments));
         };
-    });
+    })
+    // let openReplacement = (() =>{
+    //     return pttOpen.apply(this, arguments)
+    // });
 
-    startMunch();
-    startDigest();
+    // let sendReplacement = (() =>{
+    //     if(this.onreadystatechange) {
+    //         this._onreadystatechange = this.onreadystatechange;
+    //       }
+         
+    //       console.log('Request sent');
+          
+    //       this.onreadystatechange = onReadyStateChangeReplacement;
+    //       return pttSend.apply(this, arguments);
+    // });
+
+    // function onReadyStateChangeReplacement() {  
+  
+    //     console.log('Ready state changed to: ', this.readyState);
+        
+    //     if(this._onreadystatechange) {
+    //       return this._onreadystatechange.apply(this, arguments);
+    //     }
+    //   }
+
+    // window.XMLHttpRequest.prototype.open = openReplacement;  
+    // window.XMLHttpRequest.prototype.send = sendReplacement;
+    // let startMunch = (() => {
+    //     window.XMLHttpRequest.prototype.open = function() {
+    //         console.log( arguments );
+    //         return pttOpen.apply(this, [].slice.call(arguments));
+    //     };
+    // });
+
+    // let startDigest = (() => {
+    //     window.XMLHttpRequest.prototype.send = function() {
+    //         console.log( arguments );
+    //         return pttSend.apply(this, [].slice.call(arguments));
+    //     };
+    // });
+
+    // startMunch();
+    // startDigest();
+
 }
 
 var init = () => Snail("");
-
-(() => {
-    let proxied = window.XMLHttpRequest.prototype.open;
-    window.XMLHttpRequest.prototype.open = function() {
-        console.log("Track Open");
-        console.log( arguments );
-        return proxied.apply(this, [].slice.call(arguments));
-    };
-})();
-
-(() => {
-    let postxied = window.XMLHttpRequest.prototype.send;
-    window.XMLHttpRequest.prototype.send = function() {
-        console.log( arguments );
-        return postxied.apply(this, [].slice.call(arguments));
-    };
-})();
-
-(() => {
-    let xmlreqc=XMLHttpRequest;
-    XMLHttpRequest = function() {
-        this.xhr = new xmlreqc();
-        return this;
-    };
-    let responds = window.XMLHttpRequest.prototype.onreadystatechangefunction;
-    window.XMLHttpRequest.prototype.onreadystatechangefunction = function() {
-        if (this.xhr.readyState == 4){
-            console.log("responds");
-            console.log(this.xhr.responseText);
-        }
-        return responds.apply(this, [].slice.call(arguments));
-    };
-})();
